72ca35e591e43ad27a136c0f2832a0e4bcf29f3e
ksy52081 <ksy52081@gmail.com>
2022-05-04 21:00:35 +0900

    [Lecture] #6.4 useEffect의 CleanUp 사용방법~ 컴포컴포넌트컴포넌트가 소멸되는 순간에 어떤 행동을 수행시키고 싶을 때

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,52 +1,27 @@
+import { func } from "prop-types";
 import {useState, useEffect} from "react";
-// import Button from "./Button.js";
-// import yyyyy from "./XXX.module.css";
+
+function Hello(){
+    useEffect(()=>{
+        console.log("created ! :)");
+        return( ()=> console.log("destroyed"));
+    },[])
+    return(
+        <h1>Hello</h1>
+    )
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  const onClick = () => {setValue(prev => prev+1);}
-  console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("CALL THE API");
-  }
-  useEffect(iRunOnlyOnce, [])
-  useEffect(()=>{
-      if (keyword !== "" && keyword.length > 5){
-      console.log("SEARCH FOR", keyword);
+    const [showing, setShowing] = useState(false);
+    const onClick = () =>{
+        setShowing((prev) => !prev);
     }
-  }, [keyword])
-  useEffect(()=>{
-    if (keyword !== "" && keyword.length > 5){
-    console.log("I run when keyword & counter changes");
-  }
-}, [keyword, counter])
-useEffect(()=>{
-  if (keyword !== "" && keyword.length > 5){
-  console.log("I run when keyword changes");
-}
-}, [keyword])
-useEffect(()=>{
-  if (keyword !== "" && keyword.length > 5){
-  console.log("I run when counter changes");
-}
-}, [counter])
-  return (
+    return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick} > click me </button>
+        <button onClick={onClick}>{showing ? "hide" : "Show" }</button>
+        {showing ? <Hello /> : null}
     </div>
   );
-  // return (
-  //   <div>
-  //     <h1 className={yyyyy.title}>Welcome back to react</h1>
-  //     <Button onClick={onClick} text={"create"}/>
-  //   </div>
-  // );
 }
 
 export default App;
