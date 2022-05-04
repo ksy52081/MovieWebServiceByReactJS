f3650b83148f7eb4a01615889477119503d36f9b
ksy52081 <ksy52081@gmail.com>
2022-05-04 20:29:38 +0900

    [Lecture] #6.2 useEffect() 사용방법~ 두번째 argument는 감지하고 싶은 state의 리스트

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -4,14 +4,24 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   const onClick = () => {setValue(prev => prev+1);}
   console.log("i run all the time");
   const iRunOnlyOnce = () => {
     console.log("CALL THE API");
   }
   useEffect(iRunOnlyOnce, [])
+  useEffect(()=>{
+      if (keyword !== "" && keyword.length > 5){
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick} > click me </button>
     </div>
