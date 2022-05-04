6c46fbbbd3f83a845aee76824719a00ee07532c9
ksy52081 <ksy52081@gmail.com>
2022-05-04 19:10:44 +0900

    [Lecture] #6.0 문제의식~Api 불러오기처럼 첫 렌더링때만 불러오고 그다음번엔 어떤 state가 변경되더라도 불러오지 않았으면 좋겠다

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -2,12 +2,20 @@ import Button from "./Button.js";
 import yyyyy from "./XXX.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {setValue(prev => prev+1);}
   return (
     <div>
-      <h1 className={yyyyy.title}>Welcome back to react</h1>
-      <Button text={"create"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick} > click me </button>
     </div>
   );
+  // return (
+  //   <div>
+  //     <h1 className={yyyyy.title}>Welcome back to react</h1>
+  //     <Button onClick={onClick} text={"create"}/>
+  //   </div>
+  // );
 }
 
 export default App;
