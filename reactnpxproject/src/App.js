f9ffffa3fe46668b081e71f1a48f2ec48210a986
ksy52081 <ksy52081@gmail.com>
2022-05-04 14:31:05 +0900

    [Lecture] #5.1 divide & conquer 를 위한 1) export default XXX 정의 2) XXX.module.css와 XXX.js 쪼개기

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,8 +1,11 @@
+import Button from "./Button.js";
+import yyyyy from "./XXX.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back to react</h1>
+      <h1 className={yyyyy.title}>Welcome back to react</h1>
+      <Button text={"create"}/>
     </div>
   );
 }
