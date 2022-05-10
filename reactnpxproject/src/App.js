09757924c2a3e42c311bbed7b3ffc91382d125bd
ksy52081 <ksy52081@gmail.com>
2022-05-10 22:00:10 +0900

    [Lecture] #7.2 코인 API를 활용하는방법, 불러오는 사이에 loading 창 띄우는법

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,41 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-  [todo, setTodo] = useState("");
-  [todos.setTodos] = useState([]);
-  const onChange = (event) => {
-    setTodo(event.target.value);
-    // console.log(event.target.value);
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return; //만일 텅텅 비어있으면 함수를 수행하지 않겠다.
-    }
-    setTodos((currentArray) => [toDo, ...currentArray]);
-    setTodo(""); //setTodo로 todo를 빈칸으로 만들어주고, 빈칸이 되어버린 todo는 input과 연결되어있으므로, 빈칸이 출력된다.
-  };
-  return (
+function App(){
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect( () => {
+        fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+        .then((Response)=> Response.json())
+        .then(json => 
+            // console.log(json));
+            setCoins(json));
+            setLoading(false);
+    } , [])
+    return (
     <div>
-      <h1> My to dos ({todos.length}) </h1>
-      <form onsubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholeder="Write your To-do"
-        ></input>
-        <button onClick={onSubmit}> Add ToDO </button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+        <h1> The Coins{loading ? "" : `(${coins.length})`}</h1>
+        {loading ? (
+            <strong>
+                Loading ... 
+            </strong> 
+            ):(
+            <ul>
+                {coins.map((coin) => (
+                    <li> 
+                        {coin.name} {coin.symbol} : {coin.quotes.USD.price} 
+                    </li>
+                ))}
+            </ul>
+            )
+        }
     </div>
-  );
+    )
 }
+
 
 export default App;
