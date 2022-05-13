0ae0d61923c7ca46a4fc7b94abce6607413798ad
ksy52081 <ksy52081@gmail.com>
2022-05-13 17:48:37 +0900

    [Challenge] Upgrade

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,53 +1,122 @@
-import { useEffect, useState } from "react";
 
-function App(){
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [money, setMoney] = useState(0);
-    const [selectedCoin, setSelectedCoin] = useState("");
-    useEffect( () => {
-        fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-        .then((Response)=> Response.json())
-        .then(json => 
-            // console.log(json));
-            setCoins(json));
-            setLoading(false);
-    } , [])
 
-    const onChangeMoney = (event) => {
-        setMoney(event.target.value);
-        console.log(event.target.value);
-    }
-    const onChangeCoin = (event) => {
-        setSelectedCoin(event.target.value);
-        console.log(event.target.value);
-        // console.log("change!")
-    }
+    import { useEffect, useState } from "react";
 
-    return (
-    <div>
-        <h1> The Coins{loading ? "" : `(${coins.length})`}</h1>
-        {loading ? (
-            <strong>
-                Loading ... 
-            </strong> 
-            ):(
-            <div>
-                <input value={money} placeholder="please write down the amount of dollars you want " onChange={onChangeMoney}></input>
-            <select onChange={onChangeCoin}>
-                {coins.map((coin) => (
-                    <option value = {coin.quotes.USD.price} key={coin.name} > 
-                        {coin.name} {coin.symbol} : {coin.quotes.USD.price} 
-                    </option>
-                ))}
-            </select>
-                <div> You Can Buy {money/selectedCoin}</div>
-            </div>
-            )
+    function App() {
+      const [loading, setLoading] = useState(true);
+      const [coins, setCoins] = useState([]);
+      const [searchOpt, setSearchOpt] = useState("name");
+      const [keyword, setKeyword] = useState("");
+      const [searched, setSearched] = useState("");
+      const [nowSearch, setNowSearch] = useState(false);
+      const [result, setResult] = useState([]);
+      const [noResult, setNoResult] = useState(false);
+      const [pay, setPay] = useState(0);
+    
+      useEffect(() => {
+        const fetchCoins = async () => {
+          const res = await fetch("https://api.coinpaprika.com/v1/tickers");
+          const json = await res.json();
+          setCoins(json);
+          setLoading(false);
+        };
+        fetchCoins();
+      }, []);
+    
+        // useEffect( () => {
+        //     fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+        //     .then((Response)=> Response.json())
+        //     .then(json => 
+        //         // console.log(json));
+        //         setCoins(json));
+        //         setLoading(false);
+        // } , [])
+
+
+      const onChangeOpt = (event) => setSearchOpt(event.target.value);
+      const onChangeValue = (event) => setKeyword(event.target.value);
+      const onSubmit = (event) => {
+        setSearched(keyword);
+        event.preventDefault();
+        let isResult = false;
+        if (searchOpt === "name") {
+          coins.map((coin) => {
+            if (coin.name.toUpperCase() === keyword.toUpperCase()) {
+              isResult = true;
+              setResult(coin);
+              setNoResult(false);
+              setNowSearch(true);
+              return;
+            }
+          });
+        } else if (searchOpt === "symbol") {
+          coins.map((coin) => {
+            if (coin.symbol.toUpperCase() === keyword.toUpperCase()) {
+              isResult = true;
+              setResult(coin);
+              setNoResult(false);
+              setNowSearch(true);
+              return;
+            }
+          });
         }
-    </div>
-    )
-}
-
-
-export default App;
+        if (!isResult) {
+          setNoResult(true);
+          setNowSearch(false);
+        }
+        setKeyword("");
+      };
+      const payment = (event) => setPay(parseFloat(event.target.value));
+    
+      return (
+        <div>
+          <h1>The Coins! ({coins.length})</h1>
+    
+          <form onSubmit={onSubmit}>
+            <select onChange={onChangeOpt}>
+              <option value="name">Name</option>
+              <option value="symbol">Symbol</option>
+            </select>
+            <input
+              type="search"
+              onChange={onChangeValue}
+              value={keyword}
+              placeholder={searchOpt}
+            />
+            <input type="submit" value="Find Coin" />
+          </form>
+    
+          {nowSearch ? (
+            <form>
+              <h2>Calculate USD to {result.symbol}</h2>
+              <input onChange={payment} type="text" placeholder="USD" />
+              <h3>
+                You can get {pay / result.quotes.USD.price} {result.symbol}
+              </h3>
+            </form>
+          ) : null}
+    
+          {noResult ? (
+            <form>
+              <h2>
+                This {searchOpt} has not found : {searched}
+              </h2>
+            </form>
+          ) : null}
+    
+          {loading ? (
+            <strong>Loading...</strong>
+          ) : (
+            <ul>
+              {coins.map((coin) => (
+                <li key={coin.id}>
+                  {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      );
+    }
+    
+    export default App;
