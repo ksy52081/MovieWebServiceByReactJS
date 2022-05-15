bec0b7aa1dbcc38c5ce8041529752d02e37d919a
ksy52081 <ksy52081@gmail.com>
2022-05-16 00:53:56 +0900

    [Lecture] #7.3 async-await 사용시 await 여러겹을 한번에 겹쳐 쓸 수 있다 await(await(await fetch(~~~)).json()) 처럼

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,122 +1,48 @@
+import { useEffect, useState } from "react";
 
-
-    import { useEffect, useState } from "react";
-
-    function App() {
-      const [loading, setLoading] = useState(true);
-      const [coins, setCoins] = useState([]);
-      const [searchOpt, setSearchOpt] = useState("name");
-      const [keyword, setKeyword] = useState("");
-      const [searched, setSearched] = useState("");
-      const [nowSearch, setNowSearch] = useState(false);
-      const [result, setResult] = useState([]);
-      const [noResult, setNoResult] = useState(false);
-      const [pay, setPay] = useState(0);
-    
-      useEffect(() => {
-        const fetchCoins = async () => {
-          const res = await fetch("https://api.coinpaprika.com/v1/tickers");
-          const json = await res.json();
-          setCoins(json);
-          setLoading(false);
-        };
-        fetchCoins();
-      }, []);
-    
-        // useEffect( () => {
-        //     fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-        //     .then((Response)=> Response.json())
-        //     .then(json => 
-        //         // console.log(json));
-        //         setCoins(json));
-        //         setLoading(false);
-        // } , [])
-
-
-      const onChangeOpt = (event) => setSearchOpt(event.target.value);
-      const onChangeValue = (event) => setKeyword(event.target.value);
-      const onSubmit = (event) => {
-        setSearched(keyword);
-        event.preventDefault();
-        let isResult = false;
-        if (searchOpt === "name") {
-          coins.map((coin) => {
-            if (coin.name.toUpperCase() === keyword.toUpperCase()) {
-              isResult = true;
-              setResult(coin);
-              setNoResult(false);
-              setNowSearch(true);
-              return;
-            }
-          });
-        } else if (searchOpt === "symbol") {
-          coins.map((coin) => {
-            if (coin.symbol.toUpperCase() === keyword.toUpperCase()) {
-              isResult = true;
-              setResult(coin);
-              setNoResult(false);
-              setNowSearch(true);
-              return;
-            }
-          });
-        }
-        if (!isResult) {
-          setNoResult(true);
-          setNowSearch(false);
-        }
-        setKeyword("");
-      };
-      const payment = (event) => setPay(parseFloat(event.target.value));
-    
-      return (
-        <div>
-          <h1>The Coins! ({coins.length})</h1>
-    
-          <form onSubmit={onSubmit}>
-            <select onChange={onChangeOpt}>
-              <option value="name">Name</option>
-              <option value="symbol">Symbol</option>
-            </select>
-            <input
-              type="search"
-              onChange={onChangeValue}
-              value={keyword}
-              placeholder={searchOpt}
-            />
-            <input type="submit" value="Find Coin" />
-          </form>
-    
-          {nowSearch ? (
-            <form>
-              <h2>Calculate USD to {result.symbol}</h2>
-              <input onChange={payment} type="text" placeholder="USD" />
-              <h3>
-                You can get {pay / result.quotes.USD.price} {result.symbol}
-              </h3>
-            </form>
-          ) : null}
-    
-          {noResult ? (
-            <form>
-              <h2>
-                This {searchOpt} has not found : {searched}
-              </h2>
-            </form>
-          ) : null}
-    
-          {loading ? (
-            <strong>Loading...</strong>
-          ) : (
-            <ul>
-              {coins.map((coin) => (
-                <li key={coin.id}>
-                  {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const res = await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
       );
-    }
-    
-    export default App;
+      const json = await res.json();
+      //   console.log(json.data.movies);
+      setMovies(json.data.movies);
+      setLoading(false);
+      console.log(movies);
+    };
+    fetchMovie();
+  }, []);
+
+  //   const fetchMovie = async () => {
+  //       const json = await( await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')).json() );
+  //   };
+
+  return (
+    <div>
+      {loading ? (
+        <h1> Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((item) => (
+            <div key={item.id}>
+              <img src={item.medium_cover_image} />
+              <h2>{item.title}</h2>
+              <p>{item.summary} </p>
+              <ul>
+                {item.genres.map((genre) => (
+                  <li key={genre}> {genre} </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
