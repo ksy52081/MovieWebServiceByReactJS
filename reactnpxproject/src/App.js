10745e727b3012c79bf9c70a6f64baedb0be4651
ksy52081 <ksy52081@gmail.com>
2022-05-16 23:19:58 +0900

    [Lecture] #7.5 <Switch>(최근에는 Routes로 변경) <Route> 사용법, react-router-dom 기본 사용법

--- reactnpxproject/src/App.js
+++ reactnpxproject/src/App.js
@@ -1,5 +1,28 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    // Switch,
+    Route,
+    Routes,
+    Switch
+} from "react-router-dom";
+import Home from "./route/Home";
+import Detail from "./route/Detail";
+
 function App() {
-  return null;
+  return (<Router>
+      <Switch>
+          <Route path="/hello">
+                <h1> Hello !!</h1>
+          </Route>
+          <Route path="/movie" >
+              <Detail />
+          </Route>
+          <Route path="/" >
+              <Home />
+          </Route>
+      </Switch>
+  </Router>);
 }
 
 export default App;
