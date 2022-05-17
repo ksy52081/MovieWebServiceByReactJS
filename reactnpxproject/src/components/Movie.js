991a5ac54537a0f161d7cf6063e75326870fc04a
ksy52081 <ksy52081@gmail.com>
2022-05-17 23:12:16 +0900

    #7.6 dynamic URL을 사용하기 위한 <Route path=/xx/:yyy> 법, Link 변경점, useParam() 함수 사용법 정리 & 추가적인 조치 필요

--- reactnpxproject/src/components/Movie.js
+++ reactnpxproject/src/components/Movie.js
@@ -2,12 +2,12 @@ import { ReactPropTypes } from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie"> {title} </Link> 
+        <Link to={`/movie/${id}`}> {title} </Link> 
         </h2>
       <p>{summary} </p>
       <ul>{genres && genres.map((genre) => <li key={genre}> {genre} </li>)}</ul>
@@ -16,6 +16,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 // Movie.PropTypes = {
+  // id:PropTypes.number.isRequired,
 //   coverImg: PropTypes.string.isRequired,
 //   title: PropTypes.string.isRequired,
 //   summary: PropTypes.string.isRequired,
--- reactnpxproject/src/route/Detail.js
+++ reactnpxproject/src/route/Detail.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1> Detail </h1>;
+  // const x = useParams();
+  // console.log(x);
+  const {id} = useParams();
+  console.log(id);
+  const [detail, setDetail] = useState("");
+  const [loading, setLoading] = useState(true);
+  
+
+const getMovie = async() => {
+  const json = await (
+    await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setDetail(json.data.movie);
+}
+
+  useEffect(() => {
+    getMovie();
+    setLoading(false);
+  }, [id])
+    return <Movie
+    key={detail.id}
+    id={detail.id}
+    coverImg={detail.medium_cover_image}
+    title={detail.title}
+    summary={detail.summary}
+    genres={detail.genres}
+  />;
 }
 
 export default Detail;
